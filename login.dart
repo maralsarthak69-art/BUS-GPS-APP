f35d30ea52f0eb92010e4d889ac8ff987187ff97
maralsarthak69-art <maralsarthak69@gmail.com>
2025-10-12 12:41:03 +0530

    Update login.dart

--- login.dart
+++ login.dart
@@ -7,7 +7,7 @@ class MyLogin extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/background.jpeg'),
             fit: BoxFit.cover,
@@ -21,110 +21,145 @@ class MyLogin extends StatelessWidget {
               decoration: BoxDecoration(
                 color: const Color(0xff4a7c89).withOpacity(0.9),
                 borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/bus_icon.jpeg',
-                height: 100,
-                ),
-                const SizedBox(height: 20),
-
-                const Text(
-                  'GPS SYSTEM',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'GPS SYSTEM',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w900,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 30),
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                  child: TextField(
-                    style: const TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      fillColor: Colors.white.withOpacity(0.5),
-                      filled: true,
-                      hintText: 'Username',
-                      hintStyle: const TextStyle(color: Colors.white70),
-                      prefixIcon: const Icon(Icons.person, color: Colors.white),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(color: Colors.white),
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 3,
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(color: Colors.white),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/images/bus_icon.jpeg',
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.cover,
                       ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextField(
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        fillColor: Colors.white.withOpacity(0.5),
+                        filled: true,
+                        hintText: 'USERNAME',
+                        hintStyle: const TextStyle(color: Colors.white70),
+                        prefixIcon: const Icon(Icons.person, color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: const BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: const BorderSide(color: Colors.white),
+                        ),
                       ),
                     ),
                   ),
                   const SizedBox(height: 20.0),
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                  child: TextField(
-                    obscureText: true,
-                    style: const TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      fillColor: Colors.white.withOpacity(0.5),
-                      filled: true,
-                      hintText: 'Password',
-                      hintStyle: const TextStyle(color: Colors.white70),
-                      prefixIcon: Icon(Icons.lock, color: Colors.white),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(color: Colors.white),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(color: Colors.white),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextField(
+                      obscureText: true,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        fillColor: Colors.white.withOpacity(0.5),
+                        filled: true,
+                        hintText: 'PASSWORD',
+                        hintStyle: const TextStyle(color: Colors.white70),
+                        prefixIcon: const Icon(Icons.lock, color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: const BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: const BorderSide(color: Colors.white),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20.0),
-
-                SizedBox(
-                  height: 50.0,
-                  width: 200.0,
-                  child: ElevatedButton(
+                  const SizedBox(height: 20.0),
+                  SizedBox(
+                    height: 50.0,
+                    width: 200.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Handle login action
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 252, 123, 18),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: const Text(
+                        'LOGIN',
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          letterSpacing: 1.0,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15.0),
+                  const Text(
+                    'Forgot Password/Username?',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white70,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  TextButton(
                     onPressed: () {
-                      // Handle login action
+                      Navigator.pushNamed(context, 'signup');
                     },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 252, 123, 18),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0)
-                      ),
-                    ),
                     child: const Text(
-                      'Login',
+                      'Create Account',
                       style: TextStyle(
-                        fontSize: 18.0,
-                        color: Colors.white,
+                        color: Color.fromARGB(255, 23, 44, 161),
+                        decoration: TextDecoration.underline,
+                        decorationColor: Color.fromARGB(255, 23, 44, 161),
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20.0),
-
-                const Text(
-                  'Forgot Password/Username?',
-                  style: TextStyle(
-                    color: Colors.white70,
-                    decoration: TextDecoration.underline,
-                  ),
-                )
-
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
   }
 }
